4e26f9bbe2c99797cb80268f13b4417e045183d8
engsoftwill <engeletwill@gmail.com>
2020-07-13 18:34:43 -0300

    criacao contexto

---
+++ EFCoreWebAPI/EFCoreWebAPI/Data/HeroContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace EFCoreWebAPI.Data
+{
+    public class HeroContext : DbContext
+    {
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=heroapp;Data Source=DESKTOP-F99HABO\SQLEXPRESS01");
+        }
+    }
+}
