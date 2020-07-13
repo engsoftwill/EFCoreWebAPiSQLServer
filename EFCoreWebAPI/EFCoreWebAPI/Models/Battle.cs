86781b6e8cd5dfb84e9eeb6a672306abaceb6be4
engsoftwill <engeletwill@gmail.com>
2020-07-13 18:36:33 -0300

    Battle Model Created

---
+++ EFCoreWebAPI/EFCoreWebAPI/Models/Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreWebAPI.Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BattleId { get; set; }
+        public DateTime Dtbegin { get; set; }
+        public DateTime Dtfinish { get; set; }
+    }
+}
