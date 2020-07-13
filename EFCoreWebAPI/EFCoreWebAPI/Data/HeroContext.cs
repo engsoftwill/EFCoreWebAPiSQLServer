6cf23b52d21c278027e0b5ec0954b9971ebc9217
engsoftwill <engeletwill@gmail.com>
2020-07-13 19:16:55 -0300

    add-Migration HeroisBatalhas-Secret command

--- EFCoreWebAPI/EFCoreWebAPI/Data/HeroContext.cs
+++ EFCoreWebAPI/EFCoreWebAPI/Data/HeroContext.cs
@@ -14,10 +14,21 @@ namespace EFCoreWebAPI.Data
         public DbSet<Hero> Heroes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Secretidentity> Secretidentitys { get; set; }
+        public DbSet<HeroBattle> HeroBattles { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=heroapp;Data Source=DESKTOP-F99HABO\SQLEXPRESS01");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroBattle>(entity =>
+            {
+                entity.HasKey(e => new { e.BattleId, e.HeroId });
+            });
         }
     }
 }
