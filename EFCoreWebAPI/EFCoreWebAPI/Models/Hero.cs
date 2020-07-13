db638ccfce1926a66195b6fd0a8c98b8248d8b50
engsoftwill <engeletwill@gmail.com>
2020-07-13 18:35:23 -0300

    Hero Model Created

---
+++ EFCoreWebAPI/EFCoreWebAPI/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace EFCoreWebAPI.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BattleId { get; set; }
+        public Battle   Battle { get; set; }
+
+    }
+}
