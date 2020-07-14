a3affd8e06e014b78fb2b739b0a91a0a7c6ce47c
engsoftwill <engeletwill@gmail.com>
2020-07-14 10:34:34 -0300

    update database with layers

--- EFCoreWebAPI/EFRepo/HeroContext.cs
+++ EFCoreWebAPI/EFRepo/HeroContext.cs
@@ -1,10 +1,9 @@
-﻿using EFCoreWebAPI.Models;
+﻿using EFCore.Dominio;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Security.Policy;
 using System.Threading.Tasks;
 
 namespace EFCore.Repo
@@ -12,17 +11,14 @@ namespace EFCore.Repo
 {
     public class HeroContext : DbContext
     {
+        public HeroContext(DbContextOptions<HeroContext> options) : base(options) { }
+
         public DbSet<Hero> Heroes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
         public DbSet<Secretidentity> Secretidentitys { get; set; }
         public DbSet<HeroBattle> HeroBattles { get; set; }
 
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=heroapp;Data Source=DESKTOP-F99HABO\SQLEXPRESS01");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ EFCoreWebAPI/EFRepo/Migrations/20200714131541_Init.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.Repo.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Battles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    BattleId = table.Column<int>(nullable: false),
+                    Dtbegin = table.Column<DateTime>(nullable: false),
+                    Dtfinish = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Heroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    BattleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heroes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeroBattles",
+                columns: table => new
+                {
+                    HeroId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroBattles", x => new { x.BattleId, x.HeroId });
+                    table.ForeignKey(
+                        name: "FK_HeroBattles_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HeroBattles_Heroes_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Secretidentitys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealName = table.Column<string>(nullable: true),
+                    HeroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secretidentitys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Secretidentitys_Heroes_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Weapons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    HeroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Weapons_Heroes_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroBattles_HeroId",
+                table: "HeroBattles",
+                column: "HeroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Secretidentitys_HeroId",
+                table: "Secretidentitys",
+                column: "HeroId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Weapons_HeroId",
+                table: "Weapons",
+                column: "HeroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeroBattles");
+
+            migrationBuilder.DropTable(
+                name: "Secretidentitys");
+
+            migrationBuilder.DropTable(
+                name: "Weapons");
+
+            migrationBuilder.DropTable(
+                name: "Battles");
+
+            migrationBuilder.DropTable(
+                name: "Heroes");
+        }
+    }
+}
