2e360a9440e5c72c3ebdea1a01e058cf3c911b2e
engsoftwill <engeletwill@gmail.com>
2020-07-15 12:44:22 -0300

    Criação Classe Repository

---
+++ EFCoreWebAPI/EFRepo/Repository/EFCoreRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Repo.Repository
+{
+    class EFCoreRepository : IEFCoreRepository
+    {
+        private readonly HeroContext _context;
+
+        public EFCoreRepository(HeroContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T Entity) where T : class
+        {
+            _context.Add(Entity);
+        }
+
+        public void Remove<T>(T Entity) where T : class
+        {
+            _context.Remove(Entity);
+        }
+        public void Update<T>(T Entity) where T : class
+        {
+            _context.Update(Entity);
+        }
+
+        public async Task<bool> SaveChangeAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        
+    }
+}
