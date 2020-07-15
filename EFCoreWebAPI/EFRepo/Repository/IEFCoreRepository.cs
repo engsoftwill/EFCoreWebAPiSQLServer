d05c69235eb758a4d1daea1e1c7d0e635e96a2af
engsoftwill <engeletwill@gmail.com>
2020-07-15 12:31:35 -0300

    Criação de Interface IEFCoreRepository

---
+++ EFCoreWebAPI/EFRepo/Repository/IEFCoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Repo.Repository
+{
+    public interface IEFCoreRepository
+    {
+        void Add<T>(T Entity) where T : class;
+        void Update<T>(T Entity) where T : class;
+        void Remove<T>(T Entity) where T : class;
+        Task<bool> SaveChangeAsync();
+    }
+}
